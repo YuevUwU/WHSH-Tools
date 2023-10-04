3f076ef9f278384a73111def879b18ac0dffe1a1
YuevUwU <96368079+YuevUwU@users.noreply.github.com>
2023-10-05 06:11:52 +0800

    feat(get_data): Add get_data and _get_data_WID

--- main.py
+++ main.py
@@ -60,3 +60,63 @@ def refresh_source():
     with open('data.json', 'w') as f:
         json.dump(data, f)
     return data
+
+
+def _get_data_WID(uid, field = "time", order = "DESC", pageNum = 0, maxRows = 20, keyword = "", flock = "-", tf = 1, auth_type = "user", timeout=60):
+    """
+    :param field:             str; ["time" | "unit" | "title" | "clicks"]; Sorted by what
+    :param order:             str; ["DESC" | "ASC"]; Descending or Ascending
+    :param pageNum:           int; Page from 0; return "Fail to query!" when < 0; NOT return any data when > totalPages
+    :param maxRows:           int; Max rows if maxRows != 0, or return ''; return as [{"pageNum":0,"maxRows":-20,"totalPages":-796}] when < 0
+    :param keyword:           str; Search by string
+    :param flock:             str; Search with unit and/or category;Format: unit_\d+-attr_\d+
+    :param tf:                int; g_show_time_format
+    :param auth_type          str; Auth type
+    :rtype                   list;
+    :subret pageNum           int; same as parameter
+    :subret maxRow            int; same as parameter
+    :subret totalPages        int; Total pages
+    :subret newsId            str; News ID for linking to content (usually int)
+    :subret top               int; 1 for the news is HOT; 0 for normal news
+    :subret time              int; Date (YYYY/MM/DD)
+    :subret attr              str; Category ID (usually int)
+    :subret attr_name         str; Category name
+    :subret title             str; Title
+    :subret title_color       str; Title color (CSS color)
+    :subret unit              str; Unit ID (usually int)
+    :subret unit_name         str; Unit name
+    :subret issuer            str; Issuer ID (usually int)
+    :subret name              str; Publisher
+    :subret clicks            int; Click count
+    :subret content_type      str; #Unknown
+    :subret content      NoneType; #Unknown
+    :subret is_sync           int; #Unknown
+    :subret d_confirm         int; #Unknown
+    :subret permission        str; >= 1 for lock and put lock.png; else 0
+    :subret news_image        str; useless
+    :subret news_image_width  int; useless
+    :subret news_image_height  int; useless
+    """
+
+    url = "https://web.whsh.tc.edu.tw/ischool/widget/site_news/news_query_json.php"
+    param = {
+        "field": field,
+        "order": order,
+        "pageNum": pageNum,
+        "maxRows": maxRows,
+        "keyword": keyword,
+        "flock": flock,
+        "uid": uid,
+        "tf": tf,
+        "auth_type": auth_type
+    }
+    return requests.post(url, data=param, timeout=timeout).json()
+
+
+def get_data(t_name, uid, field = "time", order = "DESC", pageNum = 0, maxRows = 20, keyword = "", flock = "-", tf = 1, auth_type = "user", timeout=60):
+    match t_name:
+        case "WID":
+            ret = _get_data_WID(uid, field, order, pageNum, maxRows, keyword, flock, tf, auth_type, timeout)
+        case _:
+            raise ValueError(f'Type {t_name} is currently unsupported')
+    return ret
